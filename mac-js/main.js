dc58e100f159ca86c7e42bfec4d8b469026eb933
gilchacham <47152642+gilchacham@users.noreply.github.com>
2026-08-20 09:44:11 -0300

    Add files via upload

---
+++ mac-js/main.js
@@ -0,0 +1,11 @@
+import read from "readline-sync"
+import chalk  from "chalk"
+let nome = read.question("Digite seu nome:")
+let idade =Number(read.question("Digite sua idade"))
+let altura =parseFloat(read.question("Digite a sua altura"))
+let peso =parseFloat(read.question("Digite o seu peso")) 
+console.log (`Òlá, ${nome}`);
+
+
+
+
